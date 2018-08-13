ca7db9585321b909024a2625724908987a369e90
pengyang <pengyang@cmhi.chinamobile.com>
2018-08-13 17:33:32 +0800

    commig

--- src/main/java/org/shersfy/datahub/dbexecutor/rest/DbExecutorController.java
+++ src/main/java/org/shersfy/datahub/dbexecutor/rest/DbExecutorController.java
@@ -7,6 +7,8 @@ import org.shersfy.datahub.dbexecutor.params.template.JobConfig;
 import org.shersfy.datahub.dbexecutor.service.JobServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.fastjson.JSON;
 
 @RestController
+@RefreshScope
 public class DbExecutorController extends BaseController{
 
     protected Logger LOGGER = LoggerFactory.getLogger(getClass());
+    
+    @Value("${version}")
+    private String version = "";
     
     @Resource
     private JobServices jobServices;
 
     @GetMapping("/")
     public Object index() {
-        return "Welcom database executor application";
+        return "Welcom database executor application "+ version;
     }
 
 
